45b6c6f488ac7a24e471c2c84725bcb1cdd3add1
renatobarramansa <rbvbarra@gmail.com>
2024-06-04 18:06:53 -0300

    [fix]Inserir git ignore e variaveis de ambiente

--- mvc/main.py
+++ mvc/main.py
@@ -1,11 +1,15 @@
 from controller import ClientController
+import os
+from dotenv import load_dotenv
 
 def main():
     # Defina as informações de conexão
-    server = '127.0.0.1'  # Por exemplo, 'localhost' ou '192.168.1.1'
-    username = 'sa'
-    password = 'Jogador@123'
-    database = 'testes'
+    load_dotenv()
+    
+    server = os.getenv('DB_SERVER')
+    username = os.getenv('DB_USERNAME')
+    password = os.getenv('DB_PASSWORD')
+    database = os.getenv('DB_DATABASE')
 
     # Crie uma instância do controlador e execute a aplicação
     controller = ClientController(server, username, password,database)
